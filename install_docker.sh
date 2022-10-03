701cdb9299722692d1ccb82a9bcf164c102e3b4c
yuuki_okubo <yuuki1118sss@gmail.com>
2022-10-03 20:33:53 +0900

    update install script

--- install_docker.sh
+++ install_docker.sh
@@ -3,9 +3,8 @@ sudo apt-get install ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo apt-get update
 sudo apt-get install docker-compose-plugin
-echo "Hello, World!"
