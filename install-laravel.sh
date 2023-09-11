fc8646d7ecefa9903cf2a1e1f72741667800f4a9
yuuki_okubo <yuuki1118sss@gmail.com>
2023-09-11 23:57:37 +0900

    update install-laravel.sh

--- install-laravel.sh
+++ install-laravel.sh
@@ -3,3 +3,7 @@ composer create-project laravel/laravel .
 composer require livewire/livewire
 npm install -D tailwindcss
 npx tailwindcss init
+
+# https://github.com/laravel/vite-plugin/blob/main/UPGRADE.md#migrating-from-vite-to-laravel-mix
+npm install --save-dev laravel-mix
+npm pkg delete type
